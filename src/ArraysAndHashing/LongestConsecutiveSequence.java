3ca47466898d6a4ba90875b66f7ae9ddf7cd46a3
Lemintex <57079368+Lemintex@users.noreply.github.com>
2022-09-09 00:45:15 +0100

    Added Improved Solution to LongestConsecutiveSequence(128)

--- src/ArraysAndHashing/LongestConsecutiveSequence.java
+++ src/ArraysAndHashing/LongestConsecutiveSequence.java
@@ -1,9 +1,10 @@
 package ArraysAndHashing;
 
-import java.util.Arrays;
 import java.util.HashSet;
-
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 public class LongestConsecutiveSequence {
+    // this solution gets the length of every sequence possible
+    // eg: [4, 5, 6] considers [4, 5, 6], [5, 6], and [6] as valid sequences
     public static int longestConsecutiveSequence(int[] nums) {
         // convert to array to hashset for speedy reading
         HashSet<Integer> set = new HashSet<>();
@@ -23,6 +24,35 @@ public class LongestConsecutiveSequence {
                 current++;
             }
             if (current > longest) longest = current;
+        }
+        return longest;
+    }
+
+    // this solution is improved as it doesn't check any sequences that don't start at the beginning of the sequence
+    // eg: [4, 5, 6] doesn't consider [5, 6] or [6]
+    public static int longestConsecutiveSequenceImproved(int[] nums) {
+        // convert to array to hashset for speedy reading
+        HashSet<Integer> set = new HashSet<>();
+        for (int n: nums) {
+            set.add(n);
+        }
+
+        int longest = 0;
+
+        // for each value in the set
+        for (Integer n: set) {
+
+            //if the number is not the beginning of a set, skip it
+            if (set.contains(n-1)) continue;
+            int length = 1;
+            int val = n + 1;
+
+            // count the consecutive values to the right
+            while(set.contains(val)) {
+                length++;
+                val++;
+            }
+            if (length > longest) longest = length;
         }
         return longest;
     }
